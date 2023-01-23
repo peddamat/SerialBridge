e36f3de993ba4191b4febfb8b714816bf0748ef7
Sam Peddamatham <peddamat@gmail.com>
2023-01-23 15:45:29 -0800

    Strip complete.

--- src/FirmwareUpdaterNew.ino
+++ src/FirmwareUpdaterNew.ino
@@ -363,24 +363,6 @@ void usart_stream_move(uint32_t offset)
 	}
 }
 
-
-typedef struct __attribute__((__packed__)) {
-    uint8_t command;
-    uint32_t address;
-    uint32_t arg1;
-    uint16_t payloadLength;
-
-    // payloadLenght bytes of data follows...
-} UartPacket;
-
-static const int MAX_PAYLOAD_SIZE = 1024 * 8;
-
-#define CMD_READ_FLASH 0x01
-#define CMD_WRITE_FLASH 0x02
-#define CMD_ERASE_FLASH 0x03
-#define CMD_MAX_PAYLOAD_SIZE 0x50
-#define CMD_HELLO 0x99
-
 void setup() {
     WiFi.setPins(8, 7, 4, 2);
 
@@ -391,83 +373,5 @@ void setup() {
         Serial.println(F("Failed to put the WiFi module in download mode"));
         while (true)
             ;
-    }
-}
-
-void receivePacket(UartPacket *pkt, uint8_t *payload) {
-    // Read command
-    uint8_t *p = reinterpret_cast<uint8_t *>(pkt);
-    uint16_t l = sizeof(UartPacket);
-    while (l > 0) {
-        int c = Serial.read();
-        if (c == -1)
-            continue;
-        *p++ = c;
-        l--;
-    }
-
-    // Convert parameters from network byte order to cpu byte order
-    pkt->address = fromNetwork32(pkt->address);
-    pkt->arg1 = fromNetwork32(pkt->arg1);
-    pkt->payloadLength = fromNetwork16(pkt->payloadLength);
-
-    // Read payload
-    l = pkt->payloadLength;
-    while (l > 0) {
-        int c = Serial.read();
-        if (c == -1)
-            continue;
-        *payload++ = c;
-        l--;
-    }
-}
-
-// Allocated statically so the compiler can tell us
-// about the amount of used RAM
-static UartPacket pkt;
-static uint8_t payload[MAX_PAYLOAD_SIZE];
-
-void loop2() {
-    receivePacket(&pkt, payload);
-
-    if (pkt.command == CMD_HELLO) {
-        if (pkt.address == 0x11223344 && pkt.arg1 == 0x55667788)
-            Serial.print("v10000");
-    }
-
-    if (pkt.command == CMD_MAX_PAYLOAD_SIZE) {
-        uint16_t res = toNetwork16(MAX_PAYLOAD_SIZE);
-        Serial.write(reinterpret_cast<uint8_t *>(&res), sizeof(res));
-    }
-
-    if (pkt.command == CMD_READ_FLASH) {
-        uint32_t address = pkt.address;
-        uint32_t len = pkt.arg1;
-        if (spi_flash_read(payload, address, len) != M2M_SUCCESS) {
-            Serial.println("ER");
-        } else {
-            Serial.write(payload, len);
-            Serial.print("OK");
-        }
-    }
-
-    if (pkt.command == CMD_WRITE_FLASH) {
-        uint32_t address = pkt.address;
-        uint32_t len = pkt.payloadLength;
-        if (spi_flash_write(payload, address, len) != M2M_SUCCESS) {
-            Serial.print("ER");
-        } else {
-            Serial.print("OK");
-        }
-    }
-
-    if (pkt.command == CMD_ERASE_FLASH) {
-        uint32_t address = pkt.address;
-        uint32_t len = pkt.arg1;
-        if (spi_flash_erase(address, len) != M2M_SUCCESS) {
-            Serial.print("ER");
-        } else {
-            Serial.print("OK");
-        }
     }
 }
